2017ba21d3aef8137a22e3ae8c6645f01b138182
brenda <brendawanjiru72@gmail.com>
2024-08-09 10:37:53 +0300

    added elevation using box shadow to the menu  icon widget

--- lib/widgets/menu_icon_widget.dart
+++ lib/widgets/menu_icon_widget.dart
@@ -8,9 +8,39 @@ class MenuIconWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 42,
+      height: 42,
       decoration: const BoxDecoration(
         shape: BoxShape.circle,
         color: whiteTextColor,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x197B7B7B),
+            blurRadius: 1,
+            offset: Offset(0, 0),
+            spreadRadius: 0,
+          ),BoxShadow(
+            color: Color(0x167B7B7B),
+            blurRadius: 1,
+            offset: Offset(0, 1),
+            spreadRadius: 0,
+          ),BoxShadow(
+            color: Color(0x0C7B7B7B),
+            blurRadius: 1,
+            offset: Offset(0, 2),
+            spreadRadius: 0,
+          ),BoxShadow(
+            color: Color(0x027B7B7B),
+            blurRadius: 2,
+            offset: Offset(0, 4),
+            spreadRadius: 0,
+          ),BoxShadow(
+            color: Color(0x007B7B7B),
+            blurRadius: 2,
+            offset: Offset(0, 7),
+            spreadRadius: 0,
+          )
+        ],
       ),
       child: IconButton(
         icon: const Icon(Icons.menu, color: Colors.black),
@@ -18,7 +48,7 @@ class MenuIconWidget extends StatelessWidget {
           // Handle menu button press
         },
         color: Colors.white,
-        iconSize: 30.0,
+        // iconSize: 42.0,
         padding: const EdgeInsets.all(10),
         tooltip: 'Open Menu',
       ),
