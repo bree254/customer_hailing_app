80f4f8a81bd35531b0847159543902330bb13ee6
barizicommunications <brendawanjiru72@gmail.com>
2024-10-31 14:50:57 +0300

    1.	Create custom, reusable text styles in the app’s theme configuration. 2.Created the appbar textstyles 3.	Update existing text components throughout the codebase to use these standardized app bar text styles 4. Trip History Screens with appbars updated -history_details_screen.dart -trip_history_screen.dart

--- lib/presentation/trip_history/trip_history_screen.dart
+++ lib/presentation/trip_history/trip_history_screen.dart
@@ -16,13 +16,9 @@ class _TripHistoryScreenState extends State<TripHistoryScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: const Text(
+        title:  Text(
           'History',
-          style: TextStyle(
-            color: Color(0xFF767676),
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
+          style: AppTextStyles.largeAppBarText,
         ),
         iconTheme: const IconThemeData(color: Colors.black),
         leading: IconButton(
